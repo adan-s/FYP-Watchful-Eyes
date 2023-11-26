de0fbd861274c5efd164c255e6591dbf09b0140c
hadia2001 <90563436+hadia2001@users.noreply.github.com>
2023-11-26 20:04:09 +0500

    Update Registration-form

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fyp/screens/crime-registeration-form.dart';
 import 'package:fyp/screens/login_screen.dart';
 import 'firebase_options.dart';
 void main() async {
