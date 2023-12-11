60834072fb52192d60470fb6c3c45e781d9659f3
adan-s <adansalman73@gmail.com>
2023-12-11 15:47:54 +0500

    Community Forum

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,25 @@ import 'package:fyp/screens/splashscreenweb.dart';
 import 'package:fyp/screens/user-profile.dart';
 import 'package:get/get.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: 'AIzaSyBhcF3CgybYcpRRAk3TE-ZvgFlkJRagmgU',
-      appId: '1:321253756082:web:9843f6a0837010ea788dab',
-      messagingSenderId: '321253756082',
-      projectId: 'watchfuleyes-c2a9d',
-      authDomain: 'watchfuleyes-c2a9d.firebaseapp.com',
-      databaseURL: 'https://watchfuleyes-c2a9d-default-rtdb.firebaseio.com',
-      storageBucket: 'gs://watchfuleyes-c2a9d.appspot.com',
-    ),
-  );
-  runApp(
-    const MyApp(),
-  );
+
+  // Check if Firebase is already initialized
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyBhcF3CgybYcpRRAk3TE-ZvgFlkJRagmgU',
+        appId: '1:321253756082:web:9843f6a0837010ea788dab',
+        messagingSenderId: '321253756082',
+        projectId: 'watchfuleyes-c2a9d',
+        authDomain: 'watchfuleyes-c2a9d.firebaseapp.com',
+        databaseURL: 'https://watchfuleyes-c2a9d-default-rtdb.firebaseio.com',
+        storageBucket: 'gs://watchfuleyes-c2a9d.appspot.com',
+      ),
+    );
+  }
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
