1f2fc5d0e4ee57855f831f13ba91517ea75bf498
adan-s <adansalman73@gmail.com>
2023-12-11 11:59:17 +0500

    update

--- lib/screens/post-new-item.dart
+++ lib/screens/post-new-item.dart
@@ -204,7 +204,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker_web/image_picker_web.dart';
 
 class PostNewItemPage extends StatefulWidget {
   const PostNewItemPage({Key? key}) : super(key: key);
