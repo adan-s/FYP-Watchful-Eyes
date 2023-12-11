89098707a712ee27e7d7bd8f0d4a2f8d62962bf8
adan-s <adansalman73@gmail.com>
2023-12-11 17:58:02 +0500

    Community Forum Display

--- lib/screens/community-forum.dart
+++ lib/screens/community-forum.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp/screens/crime-registeration-form.dart';
 import 'package:fyp/screens/post-new-item.dart';
@@ -80,52 +81,62 @@ class CommunityForumPage extends StatelessWidget {
 
 
 class HomeTab extends StatelessWidget {
-  final List<Post> posts = [
-    Post(
-      username: 'John Doe',
-      description: 'This is a sample post description.',
-      imageUrl: 'https://st.depositphotos.com/1444927/1881/i/450/depositphotos_18812045-stock-photo-broken-window-crime-scene.jpg',
-    ),
-    Post(
-      username: 'Alice',
-      description: 'This is a sample post description.',
-      imageUrl: 'https://st.depositphotos.com/1444927/1881/i/450/depositphotos_18812045-stock-photo-broken-window-crime-scene.jpg',
-    ),
-    Post(
-      username: 'Sara',
-      description: 'This is a sample post description.',
-      imageUrl: 'https://st.depositphotos.com/1444927/1881/i/450/depositphotos_18812045-stock-photo-broken-window-crime-scene.jpg',
-    ),
-    Post(
-      username: 'Suzi',
-      description: 'This is a sample post description.',
-      imageUrl: 'https://st.depositphotos.com/1444927/1881/i/450/depositphotos_18812045-stock-photo-broken-window-crime-scene.jpg',
-    ),
-    // Add more dummy posts as needed
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: posts.length,
-      itemBuilder: (context, index) {
-        return PostCard(post: posts[index]);
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('items').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        }
+
+        List<Post> posts = snapshot.data!.docs.map((doc) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          return Post(
+            username: data['username'] ?? 'Unknown User',
+            description: data['description'] ?? '',
+            imageUrl: data['imageUrl'] ?? '',
+            likes: data['likes'] ?? 0,
+            comments: data['comments'] ?? 0,
+          );
+        }).toList();
+
+        return ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return PostCard(post: posts[index]);
+          },
+        );
       },
     );
   }
 }
 
+
 class Post {
   final String username;
   final String description;
   final String imageUrl;
+  final int likes;
+  final int comments;
 
   Post({
     required this.username,
     required this.description,
     required this.imageUrl,
+    required this.likes,
+    required this.comments,
   });
 }
+
 
 class PostCard extends StatelessWidget {
   final Post post;
